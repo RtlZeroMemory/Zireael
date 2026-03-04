62840101303d9880daa3112d22cb8b6b25d7e412
RtlZeroMemory <58250858+RtlZeroMemory@users.noreply.github.com>
2026-03-04 16:02:33 +0400

    fix(posix): use function-pointer-safe null atomics

--- src/util/zr_assert.c
+++ src/util/zr_assert.c
@@ -10,7 +10,7 @@
 #include <stdatomic.h>
 #include <stdlib.h>
 
-static _Atomic(zr_assert_cleanup_hook_t) g_zr_assert_cleanup_hook = NULL;
+static _Atomic(zr_assert_cleanup_hook_t) g_zr_assert_cleanup_hook = 0;
 static atomic_flag g_zr_assert_fail_guard = ATOMIC_FLAG_INIT;
 
 void zr_assert_set_cleanup_hook(zr_assert_cleanup_hook_t hook) {
@@ -23,7 +23,7 @@ void zr_assert_clear_cleanup_hook(zr_assert_cleanup_hook_t hook) {
   }
 
   zr_assert_cleanup_hook_t expected = hook;
-  (void)atomic_compare_exchange_strong_explicit(&g_zr_assert_cleanup_hook, &expected, NULL, memory_order_acq_rel,
+  (void)atomic_compare_exchange_strong_explicit(&g_zr_assert_cleanup_hook, &expected, 0, memory_order_acq_rel,
                                                 memory_order_acquire);
 }
 
